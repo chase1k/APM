3231697960b7324c5cfb6d80a0505fd64a4ced02
P4ral1ax <braydenjw00@gmail.com>
2023-03-06 15:14:59 -0500

    Create File

---
+++ Project1.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#spawn() Person 1
+    #start apms, ifstat
+
+#prco_level_metrics() Person 2
+#Script integration Person2
+
+#Sys_level_metrics() Person 2
+
+#Cleanup Person 1
+#trap    
+
+#Report Person 1 and 2
+
+#Main
+
+#spawn
+#while
+    #sleep(5)
+    #proc_level_metrics
+    #sys_level_metrics
