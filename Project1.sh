94177ce9777ca06c6197c392d46f4ebc7ca87889
chase1k <chasekillorin@gmail.com>
2023-03-06 16:26:15 -0500

    bruh

--- Project1.sh
+++ Project1.sh
@@ -4,17 +4,18 @@
     #start apms, ifstat
 
 #prco_level_metrics() Person 2
-#Script integration Person2
+#Script integration Person 2
 
-#Sys_level_metrics() Person 2
+#Sys_level_metrics() Person 3 
 
 #Cleanup Person 1
 #trap    
 
-#Report Person 1 and 2
+#Report Person 2 and 3
 
 #Main
 
+IP=$1
 #spawn
 #while
     #sleep(5)
